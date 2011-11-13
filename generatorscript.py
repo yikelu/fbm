f0d8a349da5234861e344662409a58c64324b78f
Yike Lu <yikelu.home@gmail.com>
2011-11-13 01:26:25 -0500

    Added actual file generation code to generatorscript.

--- generatorscript.py
+++ generatorscript.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-datadir='~/fbmdata/'
+datadir='../../fbmdata/'
 
 def fbmmatrix(start=0,end=30,ntimes=7560,hurst=0.5):
     timescol=np.matrix(np.linspace(start,end,ntimes)[1:])
@@ -11,3 +11,23 @@ def fbmmatrix(start=0,end=30,ntimes=7560,hurst=0.5):
     gamma=0.5*(np.power(timesrow,twohurst) + np.power(timescol,twohurst)
             - np.power(np.abs(timesrow - timescol),twohurst))
     return np.linalg.cholesky(gamma)
+
+
+
+for h in np.arange(0.1, 1.00, 0.1):
+    covmat = fbmmatrix(hurst=h)
+    fn='covmat' + str(h)
+    np.save(file=datadir+fn, arr=covmat)
+    del(covmat)
+    print('finished covmat for hurst='+str(h))
+
+#print('finished covmat generation')
+
+for i in np.arange(10):
+    montecarlo=0.3*np.sqrt(1./252.)*np.random.randn(7560,10000)
+    fn='mc'+str(i)
+    np.save(file=datadir+fn, arr=montecarlo)
+    del(montecarlo)
+    print('finished montecarlo run number '+str(i))
+
+exit()
